ff9010f10b6c4e372447d3a0819479c5b7b57f2b
wooooooooook <otsu9110@naver.com>
2025-02-10 14:35:19 +0900

    chore. 테스트 페이지 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import BoardDetailPage from './pages/BoardDetailPage';
+import TestPage from './pages/TestPage';
 
 // 전역 스타일
 const globalStyle = css`
@@ -43,6 +44,7 @@ const App = () => {
             <Route path="/board" element={<BoardPage />} />
             {/* url parameter 활용하여, 자유게시판목록 디테일 들어감 */}
             <Route path="/board/:postId" element={<BoardDetailPage />} />{' '}
+            <Route path="/write" element={<TestPage />} />
           </Route>
           {/* Layout 미적용 페이지 */}
           <Route path="/" element={<StartPage />}></Route>
