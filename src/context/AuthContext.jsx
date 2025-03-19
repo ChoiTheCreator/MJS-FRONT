2f59c7a40cf9ebea4bacb9685da194cd60705404
wooooooooook <otsu9110@naver.com>
2025-03-19 20:46:39 +0900

    fix. 로그아웃 안되는 문제 수정

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,109 +1,67 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react'
+import { postLogin, postLogout, postSignup } from '../api/authApi'
+import { useCookies } from 'react-cookie'
 
-import {
-  login as loginApi,
-  logout as logoutApi,
-  signup as signupApi,
-} from '../api/authApi';
-import { useCookies } from 'react-cookie';
-
-const AuthContext = createContext();
+const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null); // 로그인한 사용자 정보 저장
-  const [accessToken, setAccessToken] = useState(null); //빠른 접근을 위해 일단 메모리 (상태에다가 저장한다)
-  const [uuid, setUuid] = useState(null);
-  const [cookies, setCookies, removeCookies] = useCookies(['accessToken', 'refreshToken', 'uuid']);
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState(null) // 로그인한 사용자 정보 저장
+  const [accessToken, setAccessToken] = useState(null) //빠른 접근을 위해 일단 메모리 (상태에다가 저장한다)
+  const [cookies, setCookies, removeCookies] = useCookies(['accessToken', 'refreshToken'])
 
-  //새로고침 시 로그인흔적을 파악하는 부분임 -> sessionStorage
-  //1. 로그인 상태 (토큰)
-  //2. uuid
+  // 새로고침 시 로그인흔적을 파악하는 부분임 -> cookies
   useEffect(() => {
-    // const storedToken = sessionStorage.getItem('accessToken');
-    // const storedUuid = localStorage.getItem('uuid');
-    const storedToken = cookies.accessToken;
-    const storedUuid = cookies.uuid;
-
-    setAccessToken(storedToken);
-    setUuid(storedUuid);
+    const storedToken = cookies.accessToken
+    setAccessToken(storedToken)
     if (storedToken) {
-      setIsLoggedIn(true);
+      setIsLoggedIn(true)
     } else {
-      setIsLoggedIn(false);
+      setIsLoggedIn(false)
     }
-  }, []);
+  }, [])
 
-  //글로벌 회원가입 함수
-  //서버에서 현재 로그인을 할때는 uuid를 주지 않으므로 따로 로컬스토리지에다가 저장한다.
   const signup = async (userData) => {
     try {
-      const data = await signupApi(userData);
-      // const uuid = data.uuid;
-      // localStorage.setItem('uuid', uuid);
-      // console.log('uuid 저장완료 ', uuid);
-      setCookies('uuid', data.uuid)
-
-      setUuid(data.uuid);
+      await postSignup(userData)
     } catch (error) {
-      console.log('x 회원가입 실패', error);
-      throw error;
+      console.error(error)
+      throw error
     }
-  };
+  }
 
-  //글로벌 로그인 함수
-  //authContext에서 loginApi(login in authApi.jsx)함수를 빌려쓴 글로벌 로그인함수 창조 -> 얘로 실제 로그인 통신함
-
-  //새로고침시 로그인 흔적 사라지는거 방지 (보안 상 http 쿠키랑 쓰는게 좋긴한데) 일단 session storage에 저장한다.
+  // 글로벌 로그인 함수
+  // authContext에서 loginApi(login in authApi.jsx)함수를 빌려쓴 글로벌 로그인함수 창조 -> 얘로 실제 로그인 통신함
+  // 새로고침시 로그인 흔적 사라지는거 방지 (보안 상 http 쿠키랑 쓰는게 좋긴한데) 일단 session storage에 저장한다.
   const login = async (userInfo) => {
     try {
-      const data = await loginApi(userInfo); //결과물로 data.accessToken과 data.refreshToken을 줄거야.
-      setAccessToken(data.accessToken);
-      setIsLoggedIn(true);
+      const data = await postLogin(userInfo) //결과물로 data.accessToken과 data.refreshToken을 줄거야.
+      setAccessToken(data.accessToken)
+      setIsLoggedIn(true)
 
       setCookies('accessToken', data.accessToken)
       setCookies('refreshToken', data.refreshToken)
-
-      // sessionStorage.setItem('accessToken', data.accessToken);
-
-      // const storedUuid = localStorage.getItem('uuid');
-      const storedUuid = cookies.uuid;
-
-      if (storedUuid) {
-        setUuid(storedUuid);
-      } else {
-        console.warn('로그인은 성공했지만, uuid가 없습니다.');
-      }
-      // console.log(
-      //   '✅ 로그인 성공 현재 사용자의 접근토큰은 메모리에 안전하게 저장되었습니다. :',
-      //   data.accessToken
-      // );
-
-      console.log('쿠키 로그 찍는다 시발', cookies.accessToken)
     } catch (error) {
-      // console.error('❌ 로그인 실패:', error);
-      throw error;
+      console.error(error)
+      throw error
     }
-  };
+  }
 
-  //글로벌 로그아웃 함수
+  // 글로벌 로그아웃 함수
   const logout = async () => {
     try {
-      await logoutApi(accessToken); // 서버에 로그아웃 요청 -> 로컬스토로지를 헤더에 넣어줘야하므로, 요래함
-
-      removeCookies('accessToken');
-      removeCookies('refreshToken');
-      // sessionStorage.removeItem('accessToken'); // 만약 localStorage에 저장했다면 제거
-      setAccessToken(null);
-      setUser(null);
-      setIsLoggedIn(false);
-      setUuid(null);
-
-      // console.log('👋 로그아웃 완료');
+      await postLogout(accessToken)
     } catch (error) {
-      // console.error('❌ 로그아웃 context logout 함수의 실패:', error);
+      console.error('로그아웃 context logout 함수의 실패:', error)
+    } finally {
+      removeCookies('accessToken')
+      removeCookies('refreshToken')
+
+      setAccessToken(null)
+      setUser(null)
+      setIsLoggedIn(false)
     }
-  };
+  }
 
   return (
     <AuthContext.Provider
@@ -112,8 +70,6 @@ export const AuthProvider = ({ children }) => {
         setIsLoggedIn,
         user,
         setUser,
-        uuid,
-        setUuid,
         login,
         logout,
         signup,
@@ -121,7 +77,7 @@ export const AuthProvider = ({ children }) => {
     >
       {children}
     </AuthContext.Provider>
-  );
-};
+  )
+}
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext)
